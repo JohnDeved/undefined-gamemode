b1d5ba353fe3e9d846b38730353c42720f59bccb
DevSec <arma.devsec@gmail.com>
2018-02-23 02:44:11 +0100

    +push route

--- client/index.js
+++ client/index.js
@@ -1,3 +1,15 @@
+const pushRoute = route => {
+  if (!mp.gui.cursor.visible) {
+    mp.game.graphics.transitionToBlurred(100)
+    mp.browser.execute(`vue.$router.push('${route}')`)
+    mp.gui.cursor.show(true, true)
+  } else {
+    mp.game.graphics.transitionFromBlurred(100)
+    mp.browser.execute(`vue.$router.push('/')`)
+    mp.gui.cursor.show(false, false)
+  }
+}
+
 mp.events.add('loadUi', sid => {
   mp.players.local.sid = sid
   mp.gui.chat.push('playerObj sid: !{Green}' + mp.players.local.sid)
@@ -12,30 +24,6 @@ mp.events.add('server_spawnCar', car => {
   mp.events.callRemote('spawnCar', car)
 })
 
-mp.keys.bind(113, true, () => {
-  if (!mp.gui.cursor.visible) {
-    mp.game.graphics.transitionToBlurred(100)
-    mp.browser.execute(`vue.$router.push('/buttons')`)
-    mp.gui.cursor.show(true, true)
-  } else {
-    mp.game.graphics.transitionFromBlurred(100)
-    mp.browser.execute(`vue.$router.push('/')`)
-    mp.gui.cursor.show(false, false)
-  }
-})
-
-mp.keys.bind(114, true, () => {
-  if (!mp.gui.cursor.visible) {
-    mp.game.graphics.transitionToBlurred(100)
-    mp.browser.execute(`vue.$router.push('/cars')`)
-    mp.gui.cursor.show(true, true)
-  } else {
-    mp.game.graphics.transitionFromBlurred(100)
-    mp.browser.execute(`vue.$router.push('/')`)
-    mp.gui.cursor.show(false, false)
-  }
-})
-
-mp.keys.bind(116, true, () => {
-  mp.browser.reload(false)
-})
+mp.keys.bind(113, true, () => pushRoute('/buttons'))
+mp.keys.bind(114, true, () => pushRoute('/cars'))
+mp.keys.bind(116, true, () => mp.browser.reload(false))
