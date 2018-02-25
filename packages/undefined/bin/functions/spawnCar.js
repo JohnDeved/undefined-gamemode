fd20fa735b0e06d57b350e25583769195b6df2f6
DevSec <arma.devsec@gmail.com>
2018-02-25 22:35:09 +0100

    + add player as vehicle owner inside vehicle spawner menu

--- packages/undefined/bin/functions/spawnCar.js
+++ packages/undefined/bin/functions/spawnCar.js
@@ -3,6 +3,7 @@ module.exports = (player, car = 'Dominator') => {
     mp.fnc.destroyCar(player, player.vehicle)
   } else {
     let veh = mp.vehicles.new(mp.joaat(car), player.position)
+    veh.owner = player.uid
     player.call('alert', [{text: `Vehicle ${car} has been spawned!`, icon: 'fa-car', type: 'success'}])
     player.putIntoVehicle(veh, -1)
   }
