45d694160e93efcb85462f2b0ee85c5e84416962
DevSec <arma.devsec@gmail.com>
2018-02-25 22:38:20 +0100

    + car unlock headsup

--- packages/undefined/bin/functions/playerStartEnterVehicle.js
+++ packages/undefined/bin/functions/playerStartEnterVehicle.js
@@ -1,3 +1,9 @@
 module.exports = (player, vehicle, seat) => {
   console.log(vehicle.getOccupant(seat))
+
+  if (vehicle.locked) {
+    if (vehicle.owner === player.uid) {
+      player.call('alert', [{text: `You can unlock you Car by pressing 'U'`}])
+    }
+  }
 }
