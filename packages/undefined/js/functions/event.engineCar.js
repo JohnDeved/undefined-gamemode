333dea7e450f54b30724b7f0c647d5d0e4fe4f82
DevSec <arma.devsec@gmail.com>
2018-03-26 20:03:21 +0200

    + need key for engine

--- packages/undefined/js/functions/event.engineCar.js
+++ packages/undefined/js/functions/event.engineCar.js
@@ -1,5 +1,10 @@
 module.exports = player => {
   if (player.vehicle) {
-    player.vehicle.engine = !player.vehicle.engine
+    if (player.vehicle.owner === player.uid) {
+      player.vehicle.engine = !player.vehicle.engine
+    } else {
+      const name = mp.vehicleInfo[player.vehicle.model].displayName
+      player.call('alert', [{text: `You dont have the keys for this ${name}!`, icon: 'fa-key', type: 'error'}])
+    }
   }
 }
