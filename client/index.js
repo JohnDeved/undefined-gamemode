854f591e5e8d42c724bace8fcc0692e139298344
DevSec <arma.devsec@gmail.com>
2018-03-27 13:05:15 +0200

    + bone testing

--- client/index.js
+++ client/index.js
@@ -2,6 +2,13 @@ mp.fnc = require('./js/functions.js')
 require('./js/keys')()
 require('./js/events')()
 
+const vehicleInfo = require('../packages/undefined/json/vehicleInfo.nolist.json')
+mp.vehicleInfo = {}
+for (let car in vehicleInfo) {
+  let carInfo = vehicleInfo[car]
+  mp.vehicleInfo[vehicleInfo[car].hash] = carInfo
+}
+
 setInterval(mp.fnc.interval, 50)
 
 if (mp.gui.cursor.visible) {
